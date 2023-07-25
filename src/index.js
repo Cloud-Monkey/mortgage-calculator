0aeceb9178888371106dfad031a394ac6e44761d
Cloud-Monkey <ironmonkeydaveb@yahoo.co.uk>
2023-07-25 14:13:45 +0100

    created and rendered navbar component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { theme } from "./theme";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
@@ -8,7 +10,10 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+      <CssBaseline />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
